6845524c9830e3e9959f330b060e9b3c9acf9c5e
Christopher Tino <christopher_tino@yahoo.com>
2019-12-05 21:54:03 -0500

    add new content blocker to handle cosmetic filters

--- ContentBlockerCosmetic/ContentBlockerRequestHandler.swift
+++ ContentBlockerCosmetic/ContentBlockerRequestHandler.swift
@@ -1,6 +1,6 @@
 //
 // ContentBlockerRequestHandler
-// ContentBlocker
+// ContentBlockerCosmetic
 //
 // Ghostery Lite for Safari
 // https://www.ghostery.com/
@@ -10,7 +10,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0
-//
+// 
 
 import Foundation
 
@@ -18,12 +18,12 @@ class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
 	/// Tells the extension to prepare for a host app’s request.
 	/// - Parameter context: Extension context
 	func beginRequest(with context: NSExtensionContext) {
-		let blockList = Constants.AssetsFolderURL?.appendingPathComponent("currentBlockList.json")
+		let blockList = Constants.AssetsFolderURL?.appendingPathComponent("cliqzCosmeticList.json")
 		let attachment = NSItemProvider(contentsOf: blockList)
 		
 		let item = NSExtensionItem()
 		item.attachments = [attachment] as? [NSItemProvider]
-
+		
 		context.completeRequest(returningItems: [item], completionHandler: { (expired) -> Void in
 			Utils.shared.logger("Successfully reloaded static block list. (Expired? \(expired))")
 		})
